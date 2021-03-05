cc15e91e02cee7072474b1f91ab7ca0a3a00ff8a
Ramachandran A <ramhero22@gmail.com>
2021-03-05 09:15:22 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const request = require('request');
+const path = require('path');
+var express = require('express');
+let mysql = require('mysql');
+var app = express();
+const PORT = process.env.PORT || 5000;
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM alerts');
+      const results = { 'results': (result) ? result.rows : null};
+      res.send(results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+ app.listen(PORT, () => {
+	console.log(`server started on port ${PORT}`);
+  });
